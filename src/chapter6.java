f4e287eeb7ce16e1b72da8acbcd7ae4e125a27ae
Tehnikumi Õpilane <opilane@tptlive.ee>
2016-11-15 12:49:03 +0200

    Initial commit

---
+++ src/chapter6.java
@@ -0,0 +1,29 @@
+/**
+ * Created by opilane on 15.11.2016.
+ */
+/*public class chapter6 {
+    public static void main(String[] args) {
+        double a = circulateArea(1.0, 2.0, 4.0, 6.0);
+        System.out.println("area is: + a");
+    }
+    //pole rekursioon vaid tagasikutsumine
+    public static double calculateArea(double xc, double yc, double xp, double yp){
+        return calculateArea
+    }
+} lõpp
+
+public class chapter6 {
+
+    public static boolan isSingleDigit(int x) {
+        return x > -10 && x < 10;
+    }
+    if (isSingleDigit(z)) {
+        System.out.println("z is small");
+    } else {
+        System.out.println("z is big");
+    }
+} lõpp
+*/
+
+
+
